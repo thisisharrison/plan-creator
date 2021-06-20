81a4a0fea43ce234aea8276a3c9cc18c92f1f1fd
thisisharrison <harrison.lau@outlook.com>
2021-06-20 15:32:49 +0800

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:jest/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  ignorePatterns: ['node_modules', 'public'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['babel', 'import', 'jsx-a11y', 'react', 'react-hooks', 'jest-dom', 'jest', 'prettier'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'jest/no-disabled-tests': 'off',
+    'jest/no-jasmine-globals': 'off',
+    'no-prototype-builtins': 'off',
+    // 'no-console': 2,
+    // 'no-debugger': 2,
+    'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    'linebreak-style': ['error', 'unix'],
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+  },
+};
